ec72372335946a972a789e206af728202fea00ef
Jesus Romero <elherzt@gmail.com>
2013-12-03 11:52:57 -0600

    some changes to registratio users with first name and last name with devise

--- cookies/db/migrate/20131202223520_add_devise_to_users.rb
+++ cookies/db/migrate/20131202223520_add_devise_to_users.rb
@@ -1,7 +1,9 @@
 class AddDeviseToUsers < ActiveRecord::Migration
   def self.up
-    change_table(:users) do |t|
+    create_table:users do |t|
       ## Database authenticatable
+      t.string :first_name
+      t.string :last_name
       t.string :email,              :null => false, :default => ""
       t.string :encrypted_password, :null => false, :default => ""
 
